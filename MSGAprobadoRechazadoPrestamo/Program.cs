e1a6d18e7ad15c890473627fadbb770b089f41f5
Winter <Winter@DESKTOP-U9PL0DD>
2024-02-23 14:14:38 -0600

    Agregar archivos de proyecto.

---
+++ MSGAprobadoRechazadoPrestamo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Solicitar al usuario el monto del préstamo
+            Console.Write("Ingrese el monto del préstamo: Q.");
+            double monto = Convert.ToDouble(Console.ReadLine());
+
+            // Solicitar al usuario su edad
+            Console.Write("Ingrese su edad: ");
+            int edadUsuario = Convert.ToInt32(Console.ReadLine());
+
+            // Verificar las condiciones para la aprobación del préstamo
+            if (monto < 5000 || edadUsuario > 60)
+            {
+                // Aprobar el préstamo
+                Console.WriteLine("¡Felicidades! Su préstamo ha sido aprobado.");
+            }
+            else
+            {
+                // Rechazar el préstamo
+                Console.WriteLine("Lo siento, su préstamo ha sido rechazado.");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Por favor, ingrese valores numéricos válidos.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error inesperado: {ex.Message}");
+        }
+    }
+}
+
